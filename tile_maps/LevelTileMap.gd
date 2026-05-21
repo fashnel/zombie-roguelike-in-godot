e02686152fb0141980ba064d0ca5fbe31b6a3f34
Вячеслав <vaceslav@MacBook-Air-Vaceslav.local>
2026-05-22 01:16:20 +0300

    Added auto camera limits depending on the TileMap size

---
+++ tile_maps/LevelTileMap.gd
@@ -0,0 +1,20 @@
+class_name LevelTileMap extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	LevelManager.ChangeTileMapBounds(GetTileMapBounds())
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func GetTileMapBounds() -> Array[Vector2]:
+	var bounds : Array[Vector2] = []
+	bounds.append(
+		Vector2(get_used_rect().position * rendering_quadrant_size)
+	)
+	bounds.append(
+		Vector2(get_used_rect().end * rendering_quadrant_size)
+	)
+	
+	return bounds
